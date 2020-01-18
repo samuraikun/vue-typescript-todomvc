e9e41c040ae69af2a5500b0366d1af10052f6039
samuraikun <tazitaziawawa@gmail.com>
2020-01-18 18:30:30 +0900

    TypeScriptで実装したTodoMVCコンポーネントの作成

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
   #app
-    #nav
-      router-link(to="/") Home
-      router-link(to="/about") About
+    //- #nav
+    //-   router-link(to="/") Home
+    //-   router-link(to="/about") About
     router-view
 </template>
 
